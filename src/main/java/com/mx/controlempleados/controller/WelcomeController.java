7159ea9a7a15774e7115d4ee455ec3e2dff1e57b
jesus.sanchez@qacg.com <Jsanch37>
2019-02-17 23:16:22 -0600

    Modificaciones

--- src/main/java/com/mx/controlempleados/controller/WelcomeController.java
+++ src/main/java/com/mx/controlempleados/controller/WelcomeController.java
@@ -1,13 +1,13 @@
 package com.mx.controlempleados.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/home")
     @ResponseBody
     String home() {
         return "Hello World!";
